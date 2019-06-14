a6a9217a5091255323a9d2f4821389f29be9a691
LailyS <laily@lailys.com>
2019-06-14 14:31:35 -0700

    fix scroll

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
           if (window.pageYOffset === 0) {
               clearInterval(this.state.intervalId);
           }
-          window.scroll(0, 660);
+          window.scroll(0, 700);
         }
       
         scrollStepW() {
