37cafba60bd66fc84ccd61e37c5f00a67729d8cc
LailyS <laily@lailys.com>
2019-06-17 13:59:05 -0700

    detect browser

--- src/App.js
+++ src/App.js
@@ -1,293 +1,296 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header'
 import Front from './components/Front'
 import Footer from './components/Footer'
-import * as Waypoint from 'react-waypoint'
 
 
 
 
 class App extends Component {
 
-  state={
-    top:'0',
-    endTop:'-10%',
-    r:"",
-    footer:0,
+  state = {
+    top: '0',
+    endTop: '-10%',
+    r: "",
+    footer: 0,
 
-        }
-      
-        componentDidMount() {
-          window.addEventListener('scroll', this.handleScroll);
-        };
-        
-        componentWillUnmount() {
-          window.removeEventListener('scroll', this.handleScroll);
-        };
-
-        handleScroll=(e)=> {
-          const p=e.composedPath()[1].scrollY
-          
-         
-         
-      
-
-if(!!window.chrome ){
-
-
-  if(navigator.userAgent.match(/iPhone/i)!== null){
-    if(navigator.userAgent.match(/iPhone/i)[0]==="iPhone"){
-      if(window.orientation===0){
-        if(p>=0 && p<90 ){
-
-          this.setState({top:'0',endTop:'-10%'})
-
-        }else if(p>91 &&p<750){
-       
-          this.setState({top:'111%'})
-          if(p>300){
-            this.setState({endTop:'96%'})
-          }
-
-
-        }else if(p>751 ) {
-
-          this.setState({top:'215%'})
-        
-        if(p>801){
-          this.setState({endTop:'198%'})
-        }
-      }
-      }else {
-  
-        if(p>=0 && p<80){
-
-          this.setState({top:'0',endTop:'-10%'})
-
-        }else if(p>81 &&p<407){
-       
-          this.setState({top:'102%'})
-          if(p>130){
-            this.setState({endTop:'96%'})
-          }
-
-
-        }else if(p>408 ) {
-
-          this.setState({top:'206%'})
-        
-        if(p>1010){
-          this.setState({endTop:'198%'})
-        }
-      }
-      }
-
-     
-    }
-  }else if(navigator.userAgent.match(/iPad/i)!==null){
-
- 
-    if(p>=0 && p<90 ){
-
-      console.log(p)
-      this.setState({top:'0'})
-
-    }else if(p>81 &&p<1150){
-       
-      
-      console.log(p)
-      this.setState({top:'103%'})
-
-    }else if(p>1150){
-       
-      
-      console.log(p)
-      this.setState({top:'205%'})
-
-    }
-}else{
-  console.log(/iPad/i.test(navigator.userAgent))   
-  if(p>=0 && p<80 ){
-
-    this.setState({top:'0',endTop:'-10%'})
-
-  }else if(p>81 &&p<1000){
- 
-    this.setState({top:'102%'})
-    if(p>300){
-      this.setState({endTop:'96%'})
-    }
-
-
-  }else if(p>1001 ) {
-
-    this.setState({top:'202%'})
-  
-  if(p>801){
-    this.setState({endTop:'198%'})
   }
-}
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  };
+
+  handleScroll = (e) => {
+    const p = e.composedPath()[1].scrollY
+
+
+
+
+
+    if (!!window.chrome) {
+
+
+      if (navigator.userAgent.match(/iPhone/i) !== null) {
+        if (navigator.userAgent.match(/iPhone/i)[0] === "iPhone") {
+          if (window.orientation === 0) {
+            if (p >= 0 && p < 90) {
+
+              this.setState({ top: '0', endTop: '-10%' })
+
+            } else if (p > 91 && p < 750) {
+
+              this.setState({ top: '111%' })
+              if (p > 300) {
+                this.setState({ endTop: '96%' })
+              }
+
+
+            } else if (p > 751) {
+
+              this.setState({ top: '215%' })
+
+              if (p > 801) {
+                this.setState({ endTop: '198%' })
+              }
+            }
+          } else {
+
+            if (p >= 0 && p < 80) {
+
+              this.setState({ top: '0', endTop: '-10%' })
+
+            } else if (p > 81 && p < 407) {
+
+              this.setState({ top: '102%' })
+              if (p > 130) {
+                this.setState({ endTop: '96%' })
+              }
+
+
+            } else if (p > 408) {
+
+              this.setState({ top: '206%' })
+
+              if (p > 1010) {
+                this.setState({ endTop: '198%' })
+              }
+            }
+          }
+
+
+        }
+      } else if (navigator.userAgent.match(/iPad/i) !== null) {
+
+
+        if (p >= 0 && p < 90) {
+
+          console.log(p)
+          this.setState({ top: '0' })
+
+        } else if (p > 81 && p < 1150) {
+
+
+          console.log(p)
+          this.setState({ top: '103%' })
+
+        } else if (p > 1150) {
+
+
+          console.log(p)
+          this.setState({ top: '205%' })
+
+        }
+      } else {
+        console.log(/iPad/i.test(navigator.userAgent))
+        if (p >= 0 && p < 80) {
+
+          this.setState({ top: '0', endTop: '-10%' })
+
+        } else if (p > 81 && p < 1000) {
+
+          this.setState({ top: '102%' })
+          if (p > 300) {
+            this.setState({ endTop: '96%' })
+          }
+
+
+        } else if (p > 1001) {
+
+          this.setState({ top: '202%' })
+
+          if (p > 801) {
+            this.setState({ endTop: '198%' })
+          }
+        }
+      }
+
+
+    } else if (/android/i.test(navigator.userAgent)) {
+
+
+
+    } else {
+
+      if (p >= 0 && p < 80) {
+
+        this.setState({ top: '0', endTop: '-10%' })
+
+      } else if (p > 81 && p < 850) {
+
+        this.setState({ top: '102%' })
+        if (p > 130) {
+          this.setState({ endTop: '96%' })
+        }
+
+
+      } else if (p > 851) {
+
+        this.setState({ top: '202%' })
+
+        if (p > 1010) {
+          this.setState({ endTop: '198%' })
+        }
+      }
+
+    }
+
+
+
+
+  };
+
+  scrollStepT() {
+
+
+    if (!!window.chrome) {
+      if (window.pageYOffset === 0) {
+        clearInterval(this.state.intervalId);
+      }
+      window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);
+    }
   }
-      
-
-          }else if(/android/i.test(navigator.userAgent)){
-          
 
 
-          }else{
-        
-            if(p>=0 && p<80){
+  scrollStepA() {
+    if (window.pageYOffset === 0) {
+      clearInterval(this.state.intervalId);
 
-              this.setState({top:'0',endTop:'-10%'})
-  
-            }else if(p>81 &&p<850){
-           
-              this.setState({top:'102%'})
-              if(p>130){
-                this.setState({endTop:'96%'})
-              }
-  
-  
-            }else if(p>851 ) {
-  
-              this.setState({top:'202%'})
-            
-            if(p>1010){
-              this.setState({endTop:'198%'})
-            }
+    }
+    else if (!!window.chrome) {
+      if (navigator.userAgent.match(/iPad/i) !== null) {
+        if (window.orientation === 0) {
+          this.setState({r:window.orientation})
+        } else {
+          this.setState({r:window.orientation})
+        }
+
+
+
+      } else if (navigator.userAgent.match(/iPhone/i) !== null) {
+        if (navigator.userAgent.match(/iPhone/i)[0] === "iPhone") {
+
+          if (window.orientation === 0) {
+
+            window.scroll(0, 600);
+          } else {
+
+
+            window.scroll(0, 376);
+
           }
 
-          }
-        
 
-         
-      
-        };       
-        
-        scrollStepT() {
-          
-        
-          if(!!window.chrome) {if (window.pageYOffset === 0) {
-              clearInterval(this.state.intervalId);
-          }
-          window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);}
+
         }
-      
+      } else {
+        window.scroll(0, 700);
+      }
 
-        scrollStepA() {
-          if (window.pageYOffset === 0) {
-            clearInterval(this.state.intervalId);
-            
+    } else if (/android/i.test(navigator.userAgent)) {
+
+    } else {
+
+      window.scroll(0, 760)
+    }
+
+
+
+
+  }
+
+  scrollStepW() {
+
+    if (window.pageYOffset === 0) {
+      clearInterval(this.state.intervalId);
+    }
+    if (navigator.userAgent.match(/iPhone/i) !== null) {
+      if (navigator.userAgent.match(/iPhone/i)[0] === "iPhone") {
+
+        if (window.orientation === 0) {
+
+          window.scroll(0, 1200);
+
+
+
+        } else if (window.orientation === 90) {
+
+
+          window.scroll(0, 2700);
+
+
         }
-         else if(!!window.chrome){
-          if(navigator.userAgent.match(/iPad/i)!==null){
-            if(window.orientation===0){
-              window.scroll(0, 1033); 
-            }else{
-              window.scroll(0, 784);
-            }
+      }
+    } else {
 
-           
+      window.scroll(0, 2835);
+    }
 
-          }else if(navigator.userAgent.match(/iPhone/i)!== null){
-            if(navigator.userAgent.match(/iPhone/i)[0]==="iPhone"){
 
-            if(window.orientation===0){
-             
-              window.scroll(0, 600);
-              } else{
-                
-             
-              window.scroll(0, 376);
 
-              }
+  }
 
-           
 
-            }}else{
-            window.scroll(0, 700);
-          }
-           
-            }else if(/android/i.test(navigator.userAgent)){
-            
-            }else{
-            
-            window.scroll(0, 760)
-            }
-          
-       
-
-       
-        }
-      
-        scrollStepW() {
-        
-          if (window.pageYOffset === 0) {
-            clearInterval(this.state.intervalId);
-        }
-          if(navigator.userAgent.match(/iPhone/i)!== null){
-            if(navigator.userAgent.match(/iPhone/i)[0]==="iPhone"){
-
-              if(window.orientation===0){
-              
-                window.scroll(0, 1200);
-             
-           
-
-              }else if(window.orientation===90){
-                
-               
-              window.scroll(0, 2700);
-             
-
-              }}}else{
-            
-              window.scroll(0, 2835);
-              }
-          
-         
-   
-        }
-       
-
-        setId=(e)=>{
-          const p=e.target.className
-          if(p==="logo"){
-            let intervalId = setInterval(this.scrollStepT());
-            this.setState({ intervalId: intervalId });
-          }else if(p==="about"){
-            let intervalId = setInterval(this.scrollStepA());
-            this.setState({ intervalId: intervalId });
-          }else if(p==="work"){
-            let intervalId = setInterval(this.scrollStepW());
-            this.setState({ intervalId: intervalId });
-          }
-        }
+  setId = (e) => {
+    const p = e.target.className
+    if (p === "logo") {
+      let intervalId = setInterval(this.scrollStepT());
+      this.setState({ intervalId: intervalId });
+    } else if (p === "about") {
+      let intervalId = setInterval(this.scrollStepA());
+      this.setState({ intervalId: intervalId });
+    } else if (p === "work") {
+      let intervalId = setInterval(this.scrollStepW());
+      this.setState({ intervalId: intervalId });
+    }
+  }
 
   render() {
     return (
       <div className="App" >
-       
-          <div className="header" style={{top:this.state.top}}>
-                <a className="logo" onClick={(e) =>this.setId(e)}>LailyS</a>
-                <div>{this.state.r}</div>
-                <a className="about" onClick={(e) =>this.setId(e)}> About</a>
-                <a className="work"  onClick={(e)=>this.setId(e)}> Work</a>
-          </div>
-          <div className="page-end" style={{top:this.state.endTop}}>
-          
-                <div className="line">.</div>
-                <div className="line">.</div>
-                <div className="line">.</div>
-          </div>
-      
-         <div>{this.state.p}</div>
-         
-     <Front/>
 
-     <Footer style={{bottom:this.state.footer}}/>
-     
+        <div className="header" style={{ top: this.state.top }}>
+          <a className="logo" onClick={(e) => this.setId(e)}>LailyS</a>
+          <div>{this.state.r}</div>
+          <a className="about" onClick={(e) => this.setId(e)}> About</a>
+          <a className="work" onClick={(e) => this.setId(e)}> Work</a>
+        </div>
+        <div className="page-end" style={{ top: this.state.endTop }}>
+
+          <div className="line">.</div>
+          <div className="line">.</div>
+          <div className="line">.</div>
+        </div>
+
+        <div>{this.state.p}</div>
+
+        <Front />
+
+        <Footer style={{ bottom: this.state.footer }} />
+
       </div>
     );
   }
--- src/components/Front.js
+++ src/components/Front.js
@@ -1,53 +1,45 @@
-import React, {Component} from 'react';
-import {Image} from 'react-bootstrap';
-import pic1 from './pic1.jpg'
-import pic2 from './pic2.jpg'
-import pic3 from './pic3.jpg'
-import pic4 from './pic4.jpg'
-import Hpic1 from './Hpic1.jpg'
-import Hpic2 from './Hpic1.jpg'
-import Hpic3 from './Hpic1.jpg'
+import React, { Component } from 'react';
 
 class Front extends Component {
 
 
-  
+
     render() {
         return (
             <div className="front" >
                 <div className="parts a">
-                <div className="title" >
-                <div className="title-a">Web</div>
-                <div className="title-b">de</div>
-                <div className="title-c">V</div>
-                <div className="title-d">eloper</div>
-               </div>
+                    <div className="title" >
+                        <div className="title-a">Web</div>
+                        <div className="title-b">de</div>
+                        <div className="title-c">V</div>
+                        <div className="title-d">eloper</div>
+                    </div>
                 </div>
-               
+
                 <div className="parts b">
-                <div className="content">
-                <span className="about-title">About Me:<br/><br/></span>
-                <br/><br/>
-               
-                <span className="about-note">I'm an entry level designer/developer based in Bayarea. I have a passion for desining Web/App and develop application.
-                <br/>
-                <br/>
-   
-                <span style={{fontSize:"120%",fontWeight:"bold", marginLeft:"40%"}}>skills:</span>
-                <br/>
-                
-                <br/>
-                CSS, Javascript, React, Redux, Bootstrap...
+                    <div className="content">
+                        <span className="about-title">About Me:<br /><br /></span>
+                        <br /><br />
+
+                        <span className="about-note">I'm an entry level designer/developer based in Bayarea. I have a passion for desining Web/App and develop application.
+                <br />
+                            <br />
+
+                            <span style={{ fontSize: "120%", fontWeight: "bold", marginLeft: "40%" }}>skills:</span>
+                            <br />
+
+                            <br />
+                            CSS, Javascript, React, Redux, Bootstrap...
                 </span>
+                    </div>
                 </div>
-                </div>
-    ]
+                ]
                 <div className="parts c">
-              <div className="card shopping">
-              <a className="project-name" href="https://list2shop.herokuapp.com/">Project:<br/>"Shopping List"</a>
-              </div>
-                <div className="card-shadow"></div>
-                
+                    <div className="card shopping">
+                        <a className="project-name" href="https://list2shop.herokuapp.com/">Project:<br />"Shopping List"</a>
+                    </div>
+                    <div className="card-shadow"></div>
+
                 </div>
             </div>
         );
