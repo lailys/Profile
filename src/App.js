cd5ff804317430cdeb2233225f665e6f9b3e85c2
LailyS <laily@lailys.com>
2019-06-16 21:15:14 -0700

    detect browser

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ class App extends Component {
                 if (window.pageYOffset === 0) {
                   clearInterval(this.state.intervalId);
               }
-              window.scroll(0, 2800);
+              window.scroll(0, 2790);
 
               }}}else{
                 if (window.pageYOffset === 0) {
