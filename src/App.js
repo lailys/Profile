cba1683f7f097a89f93e430deb4604e009984240
LailyS <laily@lailys.com>
2019-06-17 12:06:39 -0700

    detect browser

--- src/App.js
+++ src/App.js
@@ -29,30 +29,31 @@ class App extends Component {
         handleScroll=(e)=> {
           const p=e.composedPath()[1].scrollY
           console.log(/iPad/i.test(navigator.userAgent))
+          console.log(/iPhone/i.test(navigator.userAgent))
           console.log(!!window.chrome)
          
          
       
 
 if(!!window.chrome ){
-  if(navigator.userAgent.match(/iPhone/i)!== null){
-    if(navigator.userAgent.match(/iPhone/i)[0]==="iPhone"){
+ 
+    if(/iPhone/i.test(navigator.userAgent)){
       if(window.orientation===0){
         if(p>=0 && p<90 ){
 
           this.setState({top:'0',endTop:'-10%'})
 
-        }else if(p>91 &&p<850){
+        }else if(p>91 &&p<750){
        
-          this.setState({top:'260%'})
+          this.setState({top:'111%'})
           if(p>300){
             this.setState({endTop:'96%'})
           }
 
 
-        }else if(p>851 ) {
+        }else if(p>751 ) {
 
-          this.setState({top:'440%'})
+          this.setState({top:'215%'})
         
         if(p>801){
           this.setState({endTop:'198%'})
@@ -84,7 +85,7 @@ if(!!window.chrome ){
 
      
     }
-  }else if(p>=0 && p<80 ){
+  else if(p>=0 && p<80 ){
 
               this.setState({top:'0',endTop:'-10%'})
   
