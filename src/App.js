d4ddfccb2374e7e62ee3e397861ae0ec09d27404
LailyS <laily@lailys.com>
2019-06-17 16:41:32 -0700

    detect browser

--- src/App.js
+++ src/App.js
@@ -33,151 +33,48 @@ class App extends Component {
          console.log(/iPhone/i.test(navigator.userAgent))
          console.log(/iPad/i.test(navigator.userAgent))
          console.log(!!window.chrome)
-
-
-
-         if(navigator.userAgent.match(/iPhone/i)!== null){
-          if(navigator.userAgent.match(/iPhone/i)[0]==="iPhone"){
-        
-        
-        
-        
-            if(window.orientation===0){
-        
-        
-        
-        
-        
-              if(p>=0 && p<62 ){
-        
-        
-        
-        
-                this.setState({top:'0',endTop:'-10%'})
-                if(p>17){
-                  this.setState({endTop:'96%'})
-                }
-          
-          
-          
-          
-              }
-          
-          
-          
-              else if(p>=62 && p<738 ){
-        
-          
-                this.setState({top:'160%'})
-              
-                if(p>652){
-                  this.setState({endTop:'198%'})
-                }else{
-                  this.setState({endTop:'96%'})
-                }
-            
-          
-          
-          
-              }else if(p>738){
-          
-          
-          
-                this.setState({top:'300%'})
-            
-                if(p>1010){
-                  this.setState({endTop:'198%'})
-                }}
-          
-        
-        
-        
-        
-        
-        
-        
-            }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-          }
-        
-        }
-
-
-
          
          if(!!window.chrome ){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  if(p>=0 && p<80){
-
-    this.setState({top:'0',endTop:'-10%'})
-    if(p>44){
-      this.setState({endTop:'96%'})
-    }
-
-  }else if(p>81 &&p<740){
- 
-    this.setState({top:'102%'})
-    
-    if(p>702){
-      this.setState({endTop:'198%'})
-    }else{
-      this.setState({endTop:'96%'})
-    }
-
-
-  }else if(p>740 ) {
-
-    this.setState({top:'206%'})
+if(/iPhone/i.test(navigator.userAgent)){
   
-  if(p>1010){
+
+
+
+console.log(navigator.userAgent.match(/iPhone/i)[0])
+
+
+
+
+
+
+if(p>=0 && p<80){
+
+  this.setState({top:'0',endTop:'-10%'})
+  if(p>44){
+    this.setState({endTop:'96%'})
+  }
+
+}else if(p>81 &&p<740){
+
+  this.setState({top:'102%'})
+  
+  if(p>702){
     this.setState({endTop:'198%'})
-  }}
+  }else{
+    this.setState({endTop:'96%'})
+  }
 
 
+}else if(p>740 ) {
 
+  this.setState({top:'206%'})
 
-
-
-
-
-
-
-
-
-
-
-         
-
-
-
+if(p>1010){
+  this.setState({endTop:'198%'})
+}}
 
 
 
@@ -216,16 +113,16 @@ class App extends Component {
 
          }
 
-
+     
       
         };   
         
         
 
 
+      
 
-
-
+    }
 
 
 
