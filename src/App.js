0f593afa8c7cc1a6950644124c352f03e934d466
LailyS <laily@lailys.com>
2019-06-17 12:13:33 -0700

    detect browser

--- src/App.js
+++ src/App.js
@@ -29,63 +29,63 @@ class App extends Component {
         handleScroll=(e)=> {
           const p=e.composedPath()[1].scrollY
           console.log(/iPad/i.test(navigator.userAgent))
-          console.log(/iPhone/i.test(navigator.userAgent))
           console.log(!!window.chrome)
          
          
       
 
-if(!!window.chrome ){
- 
-    if(/iPhone/i.test(navigator.userAgent)){
-      if(window.orientation===0){
-        if(p>=0 && p<90 ){
+ if(navigator.userAgent.match(/iPhone/i)!== null){
+            if(navigator.userAgent.match(/iPhone/i)[0]==="iPhone"){
+              if(window.orientation===0){
+                if(p>=0 && p<90 ){
 
-          this.setState({top:'0',endTop:'-10%'})
+                  this.setState({top:'0',endTop:'-10%'})
+      
+                }else if(p>91 &&p<750){
+               
+                  this.setState({top:'111%'})
+                  if(p>300){
+                    this.setState({endTop:'96%'})
+                  }
+      
+      
+                }else if(p>751 ) {
+      
+                  this.setState({top:'215%'})
+                
+                if(p>801){
+                  this.setState({endTop:'198%'})
+                }
+              }
+              }else {
+          
+                if(p>=0 && p<80){
 
-        }else if(p>91 &&p<750){
-       
-          this.setState({top:'111%'})
-          if(p>300){
-            this.setState({endTop:'96%'})
-          }
-
-
-        }else if(p>751 ) {
-
-          this.setState({top:'215%'})
-        
-        if(p>801){
-          this.setState({endTop:'198%'})
-        }
-      }
-      }else {
+                  this.setState({top:'0',endTop:'-10%'})
+      
+                }else if(p>81 &&p<407){
+               
+                  this.setState({top:'102%'})
+                  if(p>130){
+                    this.setState({endTop:'96%'})
+                  }
+      
+      
+                }else if(p>408 ) {
+      
+                  this.setState({top:'206%'})
+                
+                if(p>1010){
+                  this.setState({endTop:'198%'})
+                }
+              }
+              }
   
-        if(p>=0 && p<80){
-
-          this.setState({top:'0',endTop:'-10%'})
-
-        }else if(p>81 &&p<407){
-       
-          this.setState({top:'102%'})
-          if(p>130){
-            this.setState({endTop:'96%'})
-          }
-
-
-        }else if(p>408 ) {
-
-          this.setState({top:'206%'})
-        
-        if(p>1010){
-          this.setState({endTop:'198%'})
-        }
-      }
-      }
-
-     
-    }
-  else if(p>=0 && p<80 ){
+             
+            }
+          }else if(!!window.chrome ){
+           
+            if(p>=0 && p<80 ){
 
               this.setState({top:'0',endTop:'-10%'})
   
