7bce93fd75dbf6e79d8225d50f8087e5e4c70d85
LailyS <laily@lailys.com>
2019-07-06 15:48:24 -0700

    fix button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,22 +4,18 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer">
-            <div className="row1">
+                <div className="row1">
 
-            <a id="linkedin"  href="https://www.linkedin.com/in/lailysarvarian/" className="fa fa-linkedin">
-                
-            </a>
-       
-     
-                <a id="github"    href="https://github.com/lailys" className="fa fa-github"></a>
-              
-        
-                <a id="codepen "  href="https://codepen.io/lailys165" className="fa fa-codepen"/>
-              
-                <a id="email " href="mailto:laily@lailys.com"className="fa fa-envelope-o"/>
-               
-            </div>
-              
+                    <a id="linkedin" href="https://www.linkedin.com/in/lailysarvarian/" className="fa fa-linkedin"></a>
+
+                    <a id="github" href="https://github.com/lailys" className="fa fa-github"></a>
+
+                    <a id="codepen " href="https://codepen.io/lailys165" className="fa fa-codepen" />
+
+                    <a id="email " href="mailto:laily@lailys.com" className="fa fa-envelope-o" />
+
+                </div>
+
                 <p className="row2">© 2019 Laily Sarvarian. All rights reserved.</p>
             </div>
         );
